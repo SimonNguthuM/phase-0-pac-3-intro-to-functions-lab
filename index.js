089f0430eba28a38b1fd5722ee2289de77f896f1
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-06-21 16:16:34 +0300

    practice

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-function shout(string) {
-    return string.toUpperCase();
+function shout(string){
+  return string.toUpperCase();
+}
+function whisper(string){
+  return string.toLowerCase();
+}
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
   }
-function whisper(string) {
-    return string.toLowerCase();
+  else if(string === string.toUpperCase()){
+    return "YES INDEED!";
   }
-function logShout(string) {
-    console.log(string.toUpperCase());
+  else if(string === "Let's have dinner together!"){
+    return "I would love to!";
   }
-function logWhisper(string) {
-    console.log(string.toLowerCase());
-  }
-function sayHiToHeadphonedRoommate(string) 
-{
-    if (string === string.toLowerCase()){
-        return "I can't hear you!";
-    }
-    else if (string === string.toUpperCase()){
-        return "YES INDEED!";
-    }
-    else if ("Let's have dinner together!"){
-        return "I would love to!";
-    }
 }
